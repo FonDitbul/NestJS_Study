5f96df3f1f92033cca46f5b67486b107e0273a82
FonDitbul <niki7084@naver.com>
2021-10-06 20:01:49 +0900

    Block function

--- dist/index.js
+++ dist/index.js
@@ -15,6 +15,15 @@ const genesisBlock = new Block(0, "2020202124", "", "Hello", 123456);
 let blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
-const getNewTimeSTamp = () => Math.round(new Date().getTime() / 1000);
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"), createNewBlock("btebte"));
 console.log(blockchain);
 //# sourceMappingURL=index.js.map
