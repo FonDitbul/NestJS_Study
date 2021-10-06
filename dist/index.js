57d2fe61eddb9e7f60edb3c8306f42fea7cdb14b
FonDitbul <niki7084@naver.com>
2021-10-06 19:20:13 +0900

    types in typescript

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "gg", age = 24, gender = "male";
 const sayHi = (name, age, gender) => {
-    console.log(`Heelo ${name}, your are ${age}, your are a ${gender}`);
+    console.log(`Heelo ${name}, your are ${age}, your are a ${gender}!`);
 };
-sayHi(name, age);
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+const name = "gg",
+age = 24,
+gender = "male";
+
+const sayHi = (name: string, age: number, gender:string): void=> {
+    console.log(`Heelo ${name}, your are ${age}, your are a ${gender}!`)
+}
+
+sayHi(name, age, gender);
+
+
+export{};
