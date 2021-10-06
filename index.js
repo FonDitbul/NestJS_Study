ecf6f79be6c10f2dc03514c284eb4762c69b8792
FonDitbul <niki7084@naver.com>
2021-10-06 18:13:06 +0900

    typescript 기초

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "gg", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Heelo ${name}, your are ${age}, your are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+
+const name = "gg",
+age = 24,
+gender = "male";
+
+const sayHi = (name, age, gender?)=>{
+    console.log(`Heelo ${name}, your are ${age}, your are a ${gender}`)
+}
+
+sayHi(name, age);
+
+
+export{};
