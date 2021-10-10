cb6e24fb47f70f415b3d284f31a687e97c929b8d
FonDitbul <niki7084@naver.com>
2021-10-10 23:11:46 +0900

    새로운 앱을 위한 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController], //URL을 가져오고 함수를 실행
-  providers: [AppService],
+  controllers: [], //URL을 가져오고 함수를 실행
+  providers: [],
 })
 export class AppModule {}
