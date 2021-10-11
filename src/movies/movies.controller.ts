dd690d8086369bae6357873868052d94f6fd1d6d
FonDitbul <niki7084@naver.com>
2021-10-11 12:29:21 +0900

    more routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+    Controller,
+    Delete,
+    Get,
+    Patch,
+    Post,
+    Param, 
+    Body,
+    Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +16,34 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
-    @Get("/:id")
+    @Get("search")
+    search(@Query("year") searchingYear: string ){
+        return `WE are searching for a movie with a title made after : ${searchingYear} `
+    }
+
+    @Get(":id")
     getOne(@Param("id") id: string): string{
         return `This will return one moive with the id : ${id}`
     }
 
     @Post()
-    create() {
-        return "this will create a moive;"
+    create(@Body() movieData) {
+        console.log(movieData)
+        return movieData;
     }
 
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param('id') movieId:string){
         return `This will delete a moive with id : ${movieId}`
     }
 
-    @Patch("/:id") //일부분 업데이트
-    patch(@Param('id') movieId: string){
-        return `This will patch a moive with id : ${movieId}`
+    @Patch(":id") //일부분 업데이트
+    patch(@Param('id') movieId: string, @Body() updateData){
+        return {
+            updateMovie: movieId,
+            ...updateData
+        }
     }
+
+
 }
