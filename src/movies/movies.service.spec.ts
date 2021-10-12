431ccc67020f6d7f70bd2d08bbfd02f50d66b50d
FonDitbul <niki7084@naver.com>
2021-10-12 13:03:37 +0900

    unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -15,4 +17,70 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll()", () => {
+    it("should retrun an array", () =>{
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+
+    });
+  });
+  describe("getOne", () => {
+    it("should return a movie", ()=>{
+      service.create({
+        title:'TestMovie',
+        genres:["test"],
+        year: 2000,
+      })
+      const movie = service.getOne(1)
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1)
+    })
+    
+    it("should throw 404 error", () => {
+      try{
+        service.getOne(999);
+
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException)
+        expect(e.message).toEqual('Movie with ID 999 not found.')
+      }
+
+    })
+  })
+
+  describe("deleteOne", () => {
+    it("deletes a moive", () => {
+      service.create({
+        title:'TestMovie',
+        genres:["test"],
+        year: 2000,
+      })
+      const beforDelete = service.getAll().length
+      service.deleteOne(1)
+      const afterDelete = service.getAll().length
+      expect(afterDelete).toBeLessThan(beforDelete)
+    })
+    it("should retur a 404", () => {
+      try{
+        service.deleteOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException)
+      }
+    })
+  })
+
+  describe("create", () => {
+    it("should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title:'TestMovie',
+        genres:["test"],
+        year: 2000,
+      })
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    })
+  })
 });
