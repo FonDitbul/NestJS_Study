c41597c868204df70f590d256a3035ec0274bc31
FonDitbul <niki7084@naver.com>
2021-10-16 23:41:21 +0900

    cats, common 파일 옮기기

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    // throw new HttpException('api broken', 401);
+    console.log('hello controller');
+    return { cat: 'all cat' };
+  }
+
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    return 'one cat';
+  }
+
+  @Post()
+  creatCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'partial update cat';
+  }
+}
