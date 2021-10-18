0ff3fbcafcfb344f83281f12c1f40b52a8516b73
FonDitbul <niki7084@naver.com>
2021-10-18 23:48:16 +0900

    picktype설정

--- src/cats/dto/cats.request.dto.ts
+++ src/cats/dto/cats.request.dto.ts
@@ -1,3 +1,8 @@
+import { PickType } from '@nestjs/swagger';
 import { Cat } from '../cats.schema';
 
-export class CatRequestDto extends Cat {}
+export class CatRequestDto extends PickType(Cat, [
+  'email',
+  'name',
+  'password',
+] as const) {}
