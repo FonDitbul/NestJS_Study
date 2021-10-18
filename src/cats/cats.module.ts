3eb62d631f9cec3d25653f0d8be2837a032dfea9
FonDitbul <niki7084@naver.com>
2021-10-19 00:23:30 +0900

    repository 패턴

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,10 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CatsController } from './cats.controller';
 import { Cat, CatSchema } from './cats.schema';
 import { CatsService } from './cats.service';
-
+import { CatsRepository } from './cats.repository';
 @Module({
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, CatsRepository],
 })
 export class CatsModule {}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -4,14 +4,15 @@ import { Model } from 'mongoose';
 import { Cat } from './cats.schema';
 import { CatRequestDto } from './dto/cats.request.dto';
 import * as bcrypt from 'bcrypt';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly CatsRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.CatsRepository.existByEmail(email);
 
     if (isCatExist) {
       throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
@@ -20,7 +21,7 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.CatsRepository.create({
       email,
       name,
       password: hashedPassword,
