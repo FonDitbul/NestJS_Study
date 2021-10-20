a0d4912b50f5aa1893e780e29f71ee7e41ea1202
FonDitbul <niki7084@naver.com>
2021-10-20 14:55:01 +0900

    Comments 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import * as mongoose from 'mongoose';
 import { CatsModule } from './cats/cats.module';
 import { AuthModule } from './auth/auth.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     CatsModule,
     AuthModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
