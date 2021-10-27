a06939909a6e320e197b2236c8bf1e1edc865254
FonDitbul <niki7084@naver.com>
2021-10-27 21:50:26 +0900

    mongoose 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }),
   ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev';
+    mongoose.set('debug', DEBUG);
+  }
+}
