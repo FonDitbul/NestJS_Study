de83467aae8b45e158dc7b7f0c22f5d0872b769c
FonDitbul <niki7084@naver.com>
2021-10-28 22:12:21 +0900

    네임스페이스, Gateway 생명주기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,20 +1,42 @@
 import {
     ConnectedSocket,
     MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    OnGatewayInit,
     SubscribeMessage,
     WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+    private logger = new Logger('chat');
+
+    constructor() {
+        this.logger.log('constructor');
+    }
+
+    handleConnection(@ConnectedSocket() socket: Socket): any {
+        this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+    }
+
+    handleDisconnect(@ConnectedSocket() socket: Socket): any {
+        this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+    }
+
+    afterInit(server: any): any {
+        this.logger.log('init');
+    }
+
     @SubscribeMessage('new_user')
     handleNewUser(
         @MessageBody() username: string,
         @ConnectedSocket() socket: Socket,
     ) {
-        // console.log(username);
-        // return 'Hello world!';
         socket.emit('hello_user', 'hello ' + username);
         return 'return data';
     }
