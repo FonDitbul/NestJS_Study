49824f51582dd0cb4771cff4b047492c777c8560
FonDitbul <niki7084@naver.com>
2021-10-29 01:19:46 +0900

    DB 설계

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SocketModule } from '@nestjs/websockets/socket-module';
+import { Chatting, ChattingSchema } from './models/chattings.model';
+import { SocketSchema, Sockets as SocketModel } from './models/sockets.model';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            { name: Chatting.name, Schema: ChattingSchema },
+            { name: SocketModel.name, schema: SocketSchema },
+        ]),
+    ],
     providers: [ChatsGateway],
 })
 export class ChatsModule {}
---
+++ src/chats/models/sockets.model.ts
@@ -0,0 +1,28 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Document, SchemaOptions } from 'mongoose';
+const options: SchemaOptions = {
+    id: false,
+    collection: 'sockets',
+    timestamps: true,
+};
+
+@Schema(options)
+export class Sockets extends Document {
+    @Prop({
+        unique: true,
+        required: true,
+    })
+    @IsNotEmpty()
+    @IsString()
+    id: string;
+
+    @Prop({
+        required: true,
+    })
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+}
+
+export const SocketSchema = SchemaFactory.createForClass(Sockets);
